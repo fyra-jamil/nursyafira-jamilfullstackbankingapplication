68261ad89db8f19fd5654f1c3b1aabc527209b48
Nursyafira Jamil <32829952+fyra-jamil@users.noreply.github.com>
2022-02-18 21:56:19 +0800

    Update dal.js

--- dal.js
+++ dal.js
@@ -1,13 +1,13 @@
 const MongoClient = require('mongodb').MongoClient;
 const url         = 'mongodb://localhost:27017';
-let db            = null;
+//let db            = null;
  
 // connect to mongo
 MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
     console.log("Connected successfully to db server");
 
     // connect to myproject database
-    db = client.db('myproject');
+    let db = client.db('myproject');
 });
 
 // create user account
